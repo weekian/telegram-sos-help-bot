9314ba6ee4b2748f7bafc6634e9f436f45b01056
Wee Kian <weekian@policypal.com>
2018-02-27 03:02:33 +0800

    Configuration and module changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,10 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    url = require('url'),
-    proxy = require('proxy-middleware'),
-    browserSync = require('browser-sync').create()
+    autoprefixer = require('gulp-autoprefixer')
 
-var htmlDest = 'build/'
-var assetsDest = 'build/static'
+var htmlDest = 'public/'
+var assetsDest = 'public/static'
 
 gulp.task('scripts', function() {
     return gulp.src([
@@ -22,7 +19,6 @@ gulp.task('scripts', function() {
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
         .pipe(gulp.dest(assetsDest+'/js'))
-        .pipe(browserSync.stream())
 })
 
 var compileSASS = function (filename, options) {
@@ -30,7 +26,6 @@ var compileSASS = function (filename, options) {
         .pipe(autoprefixer('last 2 versions', '> 5%'))
         .pipe(concat(filename))
         .pipe(gulp.dest(assetsDest+'/css'))
-        .pipe(browserSync.stream())
 }
 
 gulp.task('sass', function() {
@@ -41,19 +36,6 @@ gulp.task('sass-minify', function() {
     return compileSASS('custom.min.css', {style: 'compressed'})
 })
 
-gulp.task('browser-sync', function() {
-    var proxyOptions = url.parse('http://localhost:' + process.env.PORT + '/api')
-    proxyOptions.route = '/api'
-    console.log(JSON.stringify(proxyOptions, null, 4))
-    browserSync.init({
-        server: {
-            baseDir: './build',
-            middleware: proxy(proxyOptions)
-        },
-        startPath: './index.html'
-    })
-})
-
 gulp.task('html', function(){
     return gulp.src('views/*.html')
         .pipe(gulp.dest(htmlDest))
@@ -61,7 +43,7 @@ gulp.task('html', function(){
 
 gulp.task('watch', function() {
     // Watch .html files
-    gulp.watch('views/*.html', ['html',browserSync.reload])
+    gulp.watch('views/*.html', ['html'])
     // Watch .js files
     gulp.watch('views/js/*.js', ['scripts'])
     // Watch .scss files
@@ -69,4 +51,4 @@ gulp.task('watch', function() {
 })
 
 // Default Task
-gulp.task('default', ['browser-sync', 'watch'])
+gulp.task('default', ['watch'])
