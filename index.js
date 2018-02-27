08a350678107cfab7ff1bdbceb59e042206244fb
Wee Kian <weekian@policypal.com>
2018-02-27 23:50:50 +0800

    Updated console log for starting express server

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(process.env.PORT, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT, () => console.log('Example app listening on ' + process.env.PORT))
