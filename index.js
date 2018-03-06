b0daee00f8a060faed57a49b233a7c7964deafd5
Wee Kian <weekian@policypal.com>
2018-03-06 21:11:43 +0800

    Reorganization of app

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 'use strict'
 const express = require('express')
-const app = express()
 const morgan = require('morgan')
-
-// Middlewares
 const helmet = require('helmet')
 const cookieParser = require('cookie-parser')
+const log = require('./src/utility/logger')
 
-// Routes
-const unprotectedRestEndpoints = require('./server/web/rest/unprotected/routes')
-const protectedRestEndpoints = require('./server/web/rest/protected/routes')
+// Requiring defined routes
+const publicWebRoutes = require('./src/web/rest/public/routes')
 
-// Setting configuration middlewares
+// Setting up express server
+const app = express()
+
+// Setting up middlewares
 app.use(helmet())
-app.use(morgan('tiny'))
+app.use(morgan('combined'))
 app.use(cookieParser(process.env.COOKIE_TOKEN))
 app.use(express.json())
 app.use(express.urlencoded({
     extended: true
 }))
 
-// Setting routes middlewares
-app.use('/api', unprotectedRestEndpoints)
-app.use('/api', protectedRestEndpoints)
+// Registering routes
+app.use('/api', publicWebRoutes)
 
-// Listening to predetermined port number
-app.listen(process.env.PORT, () => console.log('app listening to ' + process.env.PORT))
+// Listening to specified port
+app.listen(process.env.PORT, () => log('app listening to ' + process.env.PORT))
