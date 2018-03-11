5d20ff1e885903c7857b0e57e30f23045325df85
Wee Kian <weekian@policypal.com>
2018-03-11 20:22:27 +0800

    Configuration for Heroku

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict'
 const express = require('express')
-const morgan = require('morgan')
-const helmet = require('helmet')
-const cookieParser = require('cookie-parser')
-const log = require('./src/utility/logger')
+// const morgan = require('morgan')
+// const helmet = require('helmet')
+// const cookieParser = require('cookie-parser')
+// const log = require('./src/utility/logger')
 // const bot = require('./src/bot/telegram')
 
 // Requiring defined routes
@@ -13,13 +13,13 @@ const log = require('./src/utility/logger')
 const app = express()
 
 // Setting up middlewares
-app.use(helmet())
-app.use(morgan('combined'))
-app.use(cookieParser(process.env.COOKIE_TOKEN))
-app.use(express.json())
-app.use(express.urlencoded({
-    extended: true
-}))
+// app.use(helmet())
+// app.use(morgan('combined'))
+// app.use(cookieParser(process.env.COOKIE_TOKEN))
+// app.use(express.json())
+// app.use(express.urlencoded({
+//     extended: true
+// }))
 
 // app.use(express.static('public/build'))
 app.get('/', (req, res) => {
@@ -29,6 +29,6 @@ app.get('/', (req, res) => {
 // app.use('/api', webPublicRoutes)
 
 // Listening to specified port
-app.listen(process.env.PORT, () => log('app listening to ' + process.env.PORT))
+app.listen(process.env.PORT || 8080, () => console.log('app listening to ' + process.env.PORT || 8080))
 
 // bot.initialize(app)
